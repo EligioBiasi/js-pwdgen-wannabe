1ca22007023a97d079651c0ac954f88c3e6beebd
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-11 15:11:37 +0200

    password generator

---
+++ code.js
@@ -0,0 +1,5 @@
+const name = prompt('inserisci il tuo nome');
+const surname = prompt('inserisci il tuo cognome');
+const color = prompt ('inserisci il tuo colore preferito');
+const number = 22;
+document.getElementById('password').innerHTML = name + surname + color + number
